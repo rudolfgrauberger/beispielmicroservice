f11057eceffded3eef8d23035b124656e8680c49
timoTheus27 <timogerth@gmail.com>
2018-04-02 16:33:07 +0200

    add StudentController (handles http-logic)

---
+++ src/main/java/wg/hub/Controller/StudentController.java
@@ -0,0 +1,24 @@
+package wg.hub.Controller;
+
+import org.apache.catalina.valves.StuckThreadDetectionValve;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import wg.hub.Entity.Student;
+import wg.hub.Service.StudentService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Collection<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+}
