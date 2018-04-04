11722495e7e1a4408aba26f8ff977dc8277ee9ae
timoTheus27 <timogerth@gmail.com>
2018-04-04 22:59:54 +0200

    get student by id from service layer and pass it to view

--- src/main/java/wg/hub/Controller/StudentController.java
+++ src/main/java/wg/hub/Controller/StudentController.java
@@ -2,6 +2,7 @@ package wg.hub.Controller;
 
 import org.apache.catalina.valves.StuckThreadDetectionValve;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +21,10 @@ public class StudentController {
     @RequestMapping(method = RequestMethod.GET)
     public Collection<Student> getAllStudents() {
         return studentService.getAllStudents();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Student getStudentById(@PathVariable("id") int id) {
+        return studentService.getStudentById(id);
     }
 }
